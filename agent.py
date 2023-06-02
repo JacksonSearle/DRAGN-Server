27886d851f17b487e54fa8e52b01b30086d1125f
Jackson Searle <jacksonsearle24@gmail.com>
2023-06-02 15:51:07 -0600

    prompting functions

--- agent.py
+++ agent.py
@@ -51,3 +51,66 @@ class Agent:
             self.memory_stream[i].last_access = time
             retrieve.append(self.memory_stream[i])
         return retrieve
+    
+    def update_summary_description(self):
+        # Name
+        name = f'Name: {self.name} (age: {self.age})'
+
+        # Innate traits
+        innate_traits = f'Innate traits: {self.innate_traits}'
+
+        # Core characteristics
+        core_characteristics = self.summary_description_prompt(time, 'core characteristics')
+
+        # Current daily occupation
+        daily_occupation = self.summary_description_prompt(time, 'current daily occupation')
+
+        # Feelings about his recent progress in life
+        recent_progress_in_life = self.summary_description_prompt(time, 'feelings about his recent progress in life')
+
+        self.summary_description = '\n'.join(name, 
+                                             innate_traits, 
+                                             core_characteristics, 
+                                             daily_occupation, 
+                                             recent_progress_in_life)
+
+    def summary_description_prompt(self, time, text):
+        # Generate prompt
+        query = f'How would one describe {self.name}\'s {text} given the following statements?'
+        memories = self.retrieve_memories(time, query)
+        prompt = '\n'.join([query] + [memory.description for memory in memories])
+        
+        # Query model
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+        response_text = model(messages)
+        return response_text
+
+    def format_status(self):
+        return f'{self.name}\'s status: {self.status}'
+    
+    def format_description(self):
+        return f'Observation: {self.name}'
+    
+    def react(self, time, memory):
+        # Generate prompt
+        memories = self.retrieve_memories(time, memory.description)
+        relevant_context = '\n'.join([memory.description for memory in memories])
+        question = f'Based on the context above, give a json dictionary object with "react": bool and "interact": string. It will decide whether the character should react to the observation, and if they reacted, how they would interact with the object'
+        prompt = '\n'.join(self.summary_description, time_prompt(time), self.format_status(), memory.format_description(), relevant_context, question)
+        
+        # Query model
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+        response_text = model(messages)
+
+        # TODO: Ensure it's a json or reprompt
+        
+        # Parse output to json
+        dictionary = json.loads(response_text)
+
+        return dictionary['react'], dictionary['interact']
