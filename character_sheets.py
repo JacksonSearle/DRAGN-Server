2cd7cee9f94062e5409d45154b04fccde984cd73
Jackson Searle <jacksonsearle24@gmail.com>
2023-06-27 15:33:05 -0600

    now 10 characters

--- character_sheets.py
+++ character_sheets.py
@@ -6,24 +6,87 @@ character_sheets = [
         'vision_radius': 450,
         'waking_hours': {"up": 730, "down": 2200},
         'innate_traits': 'friendly, outgoing, hospitable',
-        'seed_memories': 'enjoys reading books; is a vegetarian'
+        'seed_memories': 'is a traveling salesman stopping by the town; wants to be a tourist and efficiently visit every location in the town'
     },
-    # {
-    #     'name': 'Bob',
-    #     'age': 28,
-    #     'position': {"x": 9, "y": 9, "z": 0},
-    #     'vision_radius': 3,
-    #     'waking_hours': {"up": 700, "down": 2300},
-    #     'innate_traits':'friendly, outgoing, hospitable',
-    #     'seed_memories': 'enjoys going fishing; reads the news a lot; enjoys investing in stocks; married to Alice'
-    # },
-    # {
-    #     'name': 'Alice',
-    #     'age': 27,
-    #     'position': {"x": 8, "y": 9, "z": 0},
-    #     'vision_radius': 3,
-    #     'waking_hours': {"up": 700, "down": 2215},
-    #     'innate_traits': 'friendly, outgoing, hospitable',
-    #     'seed_memories': 'loves to go swimming; likes to cook new foods'
-    # }
+    {
+        'name': 'Bob',
+        'age': 28,
+        'position': {"x": -396, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2300},
+        'innate_traits':'friendly, outgoing, hospitable',
+        'seed_memories': 'enjoys going fishing; reads the news a lot; enjoys investing in stocks; married to Alice; Has lived in the town far a few years; Wants to visit Hash'
+    },
+    {
+        'name': 'Alice',
+        'age': 27,
+        'position': {"x": -398, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2215},
+        'innate_traits': 'friendly, outgoing, hospitable',
+        'seed_memories': 'loves to go swimming; likes to cook new foods; married to Bob; Has lived in the town for a few years; Wants to visit Hash'
+    },
+    {
+        'name': 'Claire',
+        'age': 60,
+        'position': {"x": -400, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2215},
+        'innate_traits': 'friendly, outgoing, hospitable',
+        'seed_memories': 'married to Doug; likes to cook new foods; married to Bob; Has lived in the town for a few years; Wants to visit Hash'
+    },
+    {
+        'name': 'Doug',
+        'age': 62,
+        'position': {"x": -402, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2215},
+        'innate_traits': 'friendly, outgoing, hospitable',
+        'seed_memories': 'married to Claire; likes to cook new foods; married to Bob; Has lived in the town for a few years; Wants to visit Hash'
+    },
+    {
+        'name': 'Eve',
+        'age': 62,
+        'position': {"x": -404, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2215},
+        'innate_traits': 'friendly, outgoing, hospitable',
+        'seed_memories': 'Is a librarian; knows about the surrounding geography, towns, etc; enjoys participating in Ginny\'s plays'
+    },
+    {
+        'name': 'Ginny',
+        'age': 62,
+        'position': {"x": -406, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2215},
+        'innate_traits': 'friendly, outgoing, hospitable',
+        'seed_memories': 'is the local storyteller and musician; has a daily show at the square; sometimes invites others to join in as actors for a play; has an uncle named Bob'
+    },
+    {
+        'name': 'Hash',
+        'age': 62,
+        'position': {"x": -408, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2215},
+        'innate_traits': 'friendly, outgoing, hospitable',
+        'seed_memories': 'is an explorer; wants to make a map of the region; he is visiting the town after injuring his leg; He doesn\'t move much; wants to study the library'
+    },
+    {
+        'name': 'Irene',
+        'age': 62,
+        'position': {"x": -410, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2215},
+        'innate_traits': 'friendly, outgoing, hospitable',
+        'seed_memories': 'is the local witch; lives outside of town; has had a grudge against Claire for years; still has not cursed Claire'
+    },
+    {
+        'name': 'Joseph',
+        'age': 62,
+        'position': {"x": -412, "y": -3747, "z": 716},
+        'vision_radius': 3,
+        'waking_hours': {"up": 700, "down": 2215},
+        'innate_traits': 'friendly, outgoing, hospitable',
+        'seed_memories': 'is the handyman; spends most of his time at the shop waiting for jobs to come in; Lost his mother to the witch Irene; Finds eace in Ginny\'s music'
+    },
 ]
