ab6997cc04958918a57ec0aa877d6f6bd3d8e722
Jackson Searle <jacksonsearle24@gmail.com>
2023-07-17 15:23:32 -0600

    Larger vision radius to fit UE5 game positions

--- character_sheets.py
+++ character_sheets.py
@@ -4,7 +4,7 @@ character_sheets = [
         'age': 41,
         'spawn': 'destination/village/Frank_house',
         'position': {"x": -394, "y": -3747, "z": 716},
-        'vision_radius': 10,
+        'vision_radius': 200,
         'waking_hours': {"up": 730, "down": 2200},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' is a traveling salesman stopping by the town; wants to be a tourist and efficiently visit every location in the town'
@@ -14,7 +14,7 @@ character_sheets = [
         'age': 28,
         'spawn': 'destination/village/A&B_house',
         'position': {"x": -396, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2300},
         'innate_traits':'friendly, outgoing, hospitable',
         'seed_memories': ' enjoys going fishing; reads the news a lot; enjoys investing in stocks; is married to Alice; has lived in the town for a few years; wants to visit Hash'
@@ -24,7 +24,7 @@ character_sheets = [
         'age': 27,
         'spawn': 'destination/village/A&B_house',
         'position': {"x": -398, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2215},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' loves to go swimming; likes to cook new foods; is married to Bob; has lived in the town for a few years; wants to visit Hash'
@@ -34,7 +34,7 @@ character_sheets = [
         'age': 60,
         'spawn': 'destination/village/Claire&Doug_house',
         'position': {"x": -400, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2215},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' is married to Doug; likes to visit the library together with Doug; has lived in town since its founding; has a grandson Bob'
@@ -44,7 +44,7 @@ character_sheets = [
         'age': 62,
         'spawn': 'destination/village/Claire&Doug_house',
         'position': {"x": -402, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2215},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' is married to Claire; likes to visit the library together with Claire; has lived in town since its founding; has a grandson Bob'
@@ -54,7 +54,7 @@ character_sheets = [
         'age': 62,
         'spawn': 'destination/village/Eve_house',
         'position': {"x": -404, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2215},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' is a librarian; knows about the surrounding geography, towns, etc; enjoys participating in Ginny\'s plays'
@@ -64,7 +64,7 @@ character_sheets = [
         'age': 62,
         'spawn': 'destination/village/Ginny_house',
         'position': {"x": -406, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2215},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' is the local storyteller and musician; has a daily show at the square; sometimes invites others to join in as actors for a play; has an uncle named Bob'
@@ -74,7 +74,7 @@ character_sheets = [
         'age': 62,
         'spawn': 'destination/village/Hash_house',
         'position': {"x": -408, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2215},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' is an explorer; wants to make a map of the region; is visiting the town after injuring his leg; doesn\'t move much; wants to study the library'
@@ -84,7 +84,7 @@ character_sheets = [
         'age': 62,
         'spawn': 'destination/village/Irene_house',
         'position': {"x": -410, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2215},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' is the local witch; lives outside of town; has had a grudge against Claire for years; still has not cursed Claire'
@@ -94,7 +94,7 @@ character_sheets = [
         'age': 62,
         'spawn': 'destination/village/Joseph_house',
         'position': {"x": -412, "y": -3747, "z": 716},
-        'vision_radius': 3,
+        'vision_radius': 200,
         'waking_hours': {"up": 700, "down": 2215},
         'innate_traits': 'friendly, outgoing, hospitable',
         'seed_memories': ' is the handyman; spends most of his time at the shop waiting for jobs to come in; lost his mother to the witch Irene; finds peace in Ginny\'s music'
