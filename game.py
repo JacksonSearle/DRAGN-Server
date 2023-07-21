db637ef10ae436224510814c304a2ccc435011af
logicalpencils <82343982+logicalpencils@users.noreply.github.com>
2023-07-21 13:50:00 -0600

    debug and clean up various location lookups

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-import os
 from util import *
 from multiprocessing import Pool
 from pathlib import Path
@@ -6,7 +5,7 @@ import json
 import pickle
 from agent import Agent
 from memory import Memory
-from tree import get_all_nodes, build_tree
+from tree import build_tree
 from character_sheets import character_sheets
 
 from config import path
@@ -16,23 +15,18 @@ class Game:
     def __init__(self, time_step):
         self.time_step = time_step
         self.time = set_start_time(2023, 5, 24, 7, 0, 0)
+
         with open(Path(path + 'world_tree.json'), 'r') as file:
             self.root = build_tree(json.load(file))
-        self.places = get_all_nodes(self.root)
-        self.populate_lookup_places()
+        self.places = {self.root.path: self.root}
+        self.lookup_places(self.root)
         self.agents = self.make_agents()
         self.get_save_index()
 
-    def populate_lookup_places(self):
-        lookup_places = {}
-        self.lookup(self.root, lookup_places)
-        self.lookup_places = lookup_places
-
-    def lookup(self, node, lookup_places):
-        lookup_places[node.path] = node
-        for child in node.children:
-            self.lookup(child, lookup_places)
-
+    def lookup_places(self,node):
+        if not node.children: self.places[node.path] = node
+        for child in node.children: self.places.update(self.lookup_places(child))
+    
     def initial_json(self):
         data = {}
         data['save_index'] = self.save_index
@@ -42,7 +36,7 @@ class Game:
             data['agents'].append({})
             data['agents'][i]['name'] = agent.name
             data['agents'][i]['status'] = agent.status
-            data['agents'][i]['destination'] = {"nameId": agent.destination.name, "location": agent.destination.location}
+            data['agents'][i]['destination'] = agent.destination.location
             data['agents'][i]['conversation'] = agent.conversation
             data['agents'][i]['spawn_location'] = agent.spawn.location
         return data
@@ -50,7 +44,7 @@ class Game:
     def make_agents(self):
         agents = []
         for character_sheet in character_sheets:
-            agents.append(Agent(character_sheet, self.time, self))
+            agents.append(Agent(character_sheet, self.time, self.places[character_sheet['spawn']]))
         return agents
 
     def update(self, data):
@@ -60,9 +54,9 @@ class Game:
             data['agents'][i]['conversation'] = agent.conversation
 
     def update_agents(self):
+        self.time = increase_time(self.time, self.time_step)
         for agent in self.agents:
             self.update_agent(agent)
-        self.time = increase_time(self.time, self.time_step)
         # with Pool(processes=10) as pool:
         #     pool.map(self.update_agent, self.agents)
         
@@ -71,18 +65,16 @@ class Game:
         timeofday = get_timeofday(self.time)
         if timeofday == agent.waking_hours["up"]: 
             agent.plan_day(self.time)
-            agent.plan_hour(self.time)
-            agent.plan_next(self.time)
             self.execute_plan(agent)
+        #Call reflection if agent is going to bed, or call hour/minute plans if during waking hours
         elif timeofday == agent.waking_hours["down"]: agent.reflect(self.time)
         elif agent.waking_hours["up"] < timeofday < agent.waking_hours["down"]:
             if not self.perceive_objects(agent):
                 self.perceive_agents(agent)
-            if timeofday%100 == 0:
-                agent.plan_hour(self.time)
             if agent.busy_time <= 0:
                 agent.conversation = None
-                agent.plan_next(self.time)
+                if timeofday%100 == 0: agent.plan_hour(self.time)
+                else: agent.plan_next(self.time)
                 self.execute_plan(agent)
             else: agent.busy_time -= self.time_step
 
@@ -90,7 +82,7 @@ class Game:
         perceived = []
         choices = []
         #TODO: Front-end determines the objects seen by the agent
-        for place in self.places:
+        for place in self.places.values():
             if agent.is_within_range(place.location):
                 # Make an observation for that thing if its state is different or newly observed
                 if place.name not in agent.last_observed or agent.last_observed[place.name] != place.state:
@@ -103,19 +95,16 @@ class Game:
         # Choose whether or not to react to each observation
         if agent.conversation == None and len(perceived) > 0:
             react, interact = agent.react(self.time, perceived)
-            if react > len(choices)-1:
-                react = len(choices)-1
-            if react!=-1: 
+            if react>=0: 
                 agent.status = interact
-                self.execute_plan(agent, choices[react])
+                self.execute_plan(agent, choices[min(react, len(choices)-1)])
                 return True
         return False
     
     def perceive_agents(self,agent):
-        for j, other_agent in enumerate(self.agents):
+        for other_agent in self.agents:
             # Make sure an agent isn't talking to themselves
             if agent is not other_agent and agent.is_within_range(other_agent.location):
-                print("AGENT SEES ANOTHER AGENT")
                 # Make both agents see each other
                 #TODO: prompts do not consistently produce statuses that fit with this sentence structure, e.g "Bob is check on Alice", "Alice is Alice would chat"
                 description = f'{other_agent.name}\'s plan is to {other_agent.status}'
@@ -124,7 +113,7 @@ class Game:
                 
                 # Choose whether or not to react to each observation
                 react, interact = agent.react(self.time, [memory])
-                if react:
+                if react>=0:
                     agent.status = interact
                     # make a memory for the person they are talking to
                     description = agent.status
@@ -144,20 +133,18 @@ class Game:
             "state": str,
         }
         dictionary = prompt_until_success(prompt, expected_structure)
-        print("EXECUTING PLAN")
         agent.destination.state = dictionary['state']
     
     def choose_location(self,agent,root=None):
-        children = []
-        if not root: 
+        if root: location = root
+        else:
+            #Remaining at current location is an option at the first level of traversal
+            location = agent.destination
             root = self.root
-            if agent.destination != None:
-                children = [agent.destination]
-        children.extend(root.children)
 
-        choices = ''
-        for c in range(len(children)): 
-            s = f'{c+1}: {children[c].name}'
+        choices = f'0: {location.name}'
+        for c in range(len(root.children)): 
+            s = f'{c+1}: {root.children[c].name}'
             choices = '\n'.join([choices,s])
         query = f'Given the place(s) above, write a JSON dictionary object with "choice": int. Choice should be one of the indices shown above. Make its value the index of the place which is the most reasonable for {agent.name} to do the following activity: {agent.status}'
         prompt = '\n'.join([choices, query])
@@ -165,17 +152,12 @@ class Game:
             "choice": int,
         }
         dictionary = prompt_until_success(prompt, expected_structure)
-        print("CHOOSING LOCATION")
         index = dictionary["choice"]
-
-        if index < 0:
-            index = 0
-        if index > len(root.children) - 1:
-            index = len(root.children) - 1
-
-        location = root.children[index]
-        if not location.children: return location
-        return self.choose_location(agent,location)
+        
+        if not 1 <= index <= len(root.children): return location
+        location = root.children[index-1]
+        return self.choose_location(agent,location) if location.children else location
+        
 
     def conversation(self, agent, other_agent):
         # Every conversation will be 5 minutes
@@ -197,7 +179,6 @@ class Game:
             "description": str
         }
         dictionary = prompt_until_success(message, expected_structure)
-        print("CREATING CONVERSATION DESCRIPTION")
         return dictionary["description"]
     
     def get_save_index(self):
