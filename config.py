505c191276d6f7580fbdec2dec578bdb634e9f22
ROC <ROC@byu.edu>
2023-08-14 15:16:52 -0600

    Will this change config.py for everyone?

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-# import unreal
+import unreal
 #path = "D:/unreal engine/ue projects/dallinBaker-DragnTown-workspace/Content/Python/"
-path = "C:/Users/dragn/Perforce/DRAGNTownWorkspace/Content/Python/"
-path = "/mnt/c/Users/dragn/code/DRAGN-Town/DRAGNTown-Server/"
+#path = "C:/Users/dragn/Perforce/DRAGNTownWorkspace/Content/Python/"
 #path = "C:/Users/bsmit/Desktop/DRAGNTown-Server/"
 #path = "/Users/jacksonsearle/Code/Research/DRAGN-Town/Server/"
-# path = unreal.Paths.project_content_dir() + "Python/"
+path = unreal.Paths.project_content_dir() + "Python/"
 
-MODE = 'debugging'  # 'debugging' or 'testing'
+MODE = 'testing'  # 'debugging' or 'testing'
